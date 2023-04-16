a5504d8d1f0e89a2384bcb12407e16ec598de18b
Samkayzeee <lasisiabdulsamad9@gmail.com>
2023-04-16 22:43:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Learning React More
+    </div>
+  )
+}
+
+export default App
